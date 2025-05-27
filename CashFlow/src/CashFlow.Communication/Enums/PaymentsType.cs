b06490087ae552bb8451a1a4981fc3a32a27b74c
Mattheus Adhonnay <mamenezes@viceri.com.br>
2025-05-26 22:40:49 -0300

    feat: register expense endpoint

---
+++ CashFlow/src/CashFlow.Communication/Enums/PaymentsType.cs
@@ -0,0 +1,8 @@
+﻿namespace CashFlow.Communication.Enums;
+public enum PaymentsType
+{
+    Cash = 0,
+    CreditCard = 1,
+    DebitCard = 2,
+    EletronicTransfer = 3,
+}
---
+++ CashFlow/src/CashFlow.Communication/Requests/RequestRegisterExpenseJson.cs
@@ -0,0 +1,10 @@
+﻿using CashFlow.Communication.Enums;
+
+namespace CashFlow.Communication.Requests;
+public class RequestRegisterExpenseJson
+{
+    public string Title { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+    public decimal Amount { get; set; }
+    public PaymentsType PaymentType { get; set; }
+}
