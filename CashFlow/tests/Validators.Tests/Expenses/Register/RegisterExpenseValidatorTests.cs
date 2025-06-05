ece63fed2d78341bbbe83691a6f545d31a72910b
Mattheus Adhonnay <mamenezes@viceri.com.br>
2025-06-05 12:37:40 -0300

    feat: intial unit tests

---
+++ CashFlow/tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,27 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+
+namespace Validators.Tests.Expenses.Register;
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        // Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = new RequestRegisterExpenseJson
+        {
+            Title = "Test Expense",
+            Amount = 100.00m,
+            Date = DateTime.Now.AddDays(-1),
+            Description = "Test Description",
+            PaymentType= CashFlow.Communication.Enums.PaymentsType.CreditCard
+        };
+
+        // Act
+        var result = validator.Validate(request);
+
+        // Assert
+        Assert.True(result.IsValid);
+    }
+}
