910c8e5648739924093d8af00c88c4399211b5e4
Mattheus Adhonnay <mattheusadhonnaysf@hotmail.com>
2026-01-29 12:30:49 -0300

    feat: refatorando para tipo task

--- CashFlow/src/CashFlow.Api/Controllers/ExpensesController.cs
+++ CashFlow/src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -9,9 +9,7 @@ namespace CashFlow.Api.Controllers;
 public class ExpensesController : ControllerBase
 {
   [HttpPost]
-  public async Task<IActionResult> Register(
-    [FromServices] IRegisterExpenseUseCase useCase,
-    [FromBody] RequestRegisterExpenseJson request)
+  public async Task<IActionResult> Register([FromServices] IRegisterExpenseUseCase useCase, [FromBody] RequestRegisterExpenseJson request)
   {
     var response = await useCase.Execute(request);
 
