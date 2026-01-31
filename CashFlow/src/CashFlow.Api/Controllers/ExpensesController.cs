3548400eb81210e26a1645cf583efba1e79007f1
Mattheus Adhonnay <mattheusadhonnaysf@hotmail.com>
2026-01-31 13:36:56 -0300

    feat: endpoint para listar despesas

--- CashFlow/src/CashFlow.Api/Controllers/ExpensesController.cs
+++ CashFlow/src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Application.UseCases.Expenses.Register;
+﻿using CashFlow.Application.UseCases.Expenses.GetAll;
+using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
@@ -14,11 +15,23 @@ public class ExpensesController : ControllerBase
   [ProducesResponseType(typeof(ResponseRegisterExpenseJson), StatusCodes.Status400BadRequest)]
   public async Task<IActionResult> Register(
     [FromServices] IRegisterExpenseUseCase useCase,
-    [FromBody] RequestRegisterExpenseJson request
-    )
+    [FromBody] RequestRegisterExpenseJson request)
   {
     var response = await useCase.Execute(request);
 
     return Created(string.Empty, response);
+  }
+
+  [HttpGet]
+  [ProducesResponseType(typeof(ResponseExpensesJson), StatusCodes.Status200OK)]
+  [ProducesResponseType(StatusCodes.Status404NotFound)]
+  public async Task<IActionResult> GetAllExpenses([FromServices] IGetAllExpenseUseCase useCase)
+  {
+    var response = await useCase.Execute();
+
+    if (response.Expenses.Any())
+      return Ok(response);
+
+    return NotFound();
   }
 }
