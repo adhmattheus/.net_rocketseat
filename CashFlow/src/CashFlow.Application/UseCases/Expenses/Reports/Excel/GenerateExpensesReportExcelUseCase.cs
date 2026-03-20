a88c0e7997f9ef0c43cc298aa17e0fa33cddd7c5
Mattheus Adhonnay <mattheusadhonnaysf@hotmail.com>
2026-03-20 16:26:23 -0300

    feat: filtro de despesas e valores no arquivo excel

--- CashFlow/src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
+++ CashFlow/src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -1,12 +1,30 @@
-﻿using CashFlow.Domain.Reports;
+﻿using CashFlow.Domain.Enums;
+using CashFlow.Domain.Reports;
+using CashFlow.Domain.Repositories.Expenses;
 using ClosedXML.Excel;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Excel;
 
 public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUseCase
 {
+
+  private readonly IExpensesReadOnlyRepository _repository;
+
+  public GenerateExpensesReportExcelUseCase(IExpensesReadOnlyRepository repository)
+  {
+    _repository = repository;
+  }
+
   public async Task<byte[]> Execute(DateOnly month)
   {
+
+    var expenses = await _repository.FilterByMonth(month);
+
+    if (expenses.Count == 0)
+    {
+      return [];
+    }
+
     var workbook = new XLWorkbook();
 
     workbook.Author = "CashFlow";
@@ -17,10 +35,36 @@ public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUs
 
     InsertHeader(worksheet);
 
+    var raw = 2;
+    foreach (var expense in expenses)
+    {
+      worksheet.Cell($"A{raw}").Value = expense.Title;
+      worksheet.Cell($"B{raw}").Value = expense.Date;
+      worksheet.Cell($"C{raw}").Value = ConvertPaymentType(expense.PaymentType);
+      worksheet.Cell($"D{raw}").Value = expense.Amount;
+      worksheet.Cell($"E{raw}").Value = expense.Description;
+
+      raw++;
+    }
+
+    worksheet.Columns().AdjustToContents();
+
     var file = new MemoryStream();
     workbook.SaveAs(file);
 
     return file.ToArray();
+  }
+
+  private string ConvertPaymentType(PaymentType paymentType)
+  {
+    return paymentType switch
+    {
+      PaymentType.Cash => "Dinheiro",
+      PaymentType.CreditCard => "Cartão de Crédito",
+      PaymentType.DebitCard => "Cartão de Débito",
+      PaymentType.EletronicTransfer => "Transferência Bancária",
+      _ => string.Empty
+    };
   }
 
   private void InsertHeader(IXLWorksheet worksheet)
@@ -32,12 +76,13 @@ public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUs
     worksheet.Cell("E1").Value = ResourceReportGenerationMessages.DESCRIPTION;
 
     worksheet.Cells("A1:E1").Style.Font.Bold = true;
-    worksheet.Cells("A1:E1").Style.Fill.BackgroundColor = XLColor.FromHtml("#8427F5");
+
+    worksheet.Cells("A1:E1").Style.Fill.BackgroundColor = XLColor.FromHtml("#F5C2B6");
 
     worksheet.Cell("A1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
     worksheet.Cell("B1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
     worksheet.Cell("C1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
-    worksheet.Cell("D1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Right);
     worksheet.Cell("E1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+    worksheet.Cell("D1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Right);
   }
 }
