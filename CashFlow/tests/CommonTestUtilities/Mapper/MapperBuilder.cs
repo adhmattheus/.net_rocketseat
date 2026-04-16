11c16a060276263eb160f0bae8aa2eff67ddee02
Mattheus Adhonnay <mattheusadhonnaysf@hotmail.com>
2026-04-16 18:07:58 -0300

    feat: testes de unidade para useCase de registro de usuário

---
+++ CashFlow/tests/CommonTestUtilities/Mapper/MapperBuilder.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CashFlow.Application.AutoMapper;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace CommonTestUtilities.Mapper;
+
+public class MapperBuilder
+{
+  public static IMapper Build()
+  {
+    var mapperConfig = new MapperConfiguration(config =>
+    {
+      config.AddProfile(new AutoMapping());
+    }, NullLoggerFactory.Instance);
+
+    return mapperConfig.CreateMapper();
+  }
+}
